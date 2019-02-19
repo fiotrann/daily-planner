ec7d3b811dd511c0b3a1ef225e21b66a5b48277b
Fiona <fiona.tran@ixup.com>
2019-02-19 22:19:10 +1100

    #1.7

--- src/app/activities/activities.module.ts
+++ src/app/activities/activities.module.ts
@@ -5,12 +5,16 @@ import { ActivitiesRoutingModule } from './activities-routing.module';
 import { ActivitiesComponent } from './activities.component';
 import { ListActivitiesComponent } from './list-activities/list-activities.component';
 import { ViewPlanComponent } from './view-plan/view-plan.component';
+import { CustomiseFormComponent } from './customise-form/customise-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ActivitiesComponent, ListActivitiesComponent, ViewPlanComponent],
+  declarations: [ActivitiesComponent, ListActivitiesComponent, ViewPlanComponent, CustomiseFormComponent],
   imports: [
     CommonModule,
-    ActivitiesRoutingModule
+    ActivitiesRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class ActivitiesModule { }
